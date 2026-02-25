20c3e8c28a90d797b609b5369e7f7e954e50314e
Jens <jens@perun.network>
2026-02-25 17:26:43 +0100

    Move rust-witness callbacks to separate file

--- src/main.rs
+++ src/main.rs
@@ -7,86 +7,16 @@ use serde_json::json;
 
 // Generated code to go from input to witness.
 mod witness {
+    // Contains non-mangled symbols called from C
+    mod runtime;
+
     // rust_witness::witness!(dlpexample);
-    // rust_witness::witness!(sdjwt_es256_sha256_1claim);
     rust_witness::witness!(sdjwtes256sha2561claim);
 }
 
 mod keyfinder;
 mod prover;
 mod sdjwt;
-
-// Functions rust_witness normally implements as a NOP.
-mod runtime {
-    use std::ffi::{CStr, c_char, c_void};
-
-    #[unsafe(no_mangle)]
-    #[allow(non_snake_case)]
-    extern "C" fn runtime__exceptionHandler(_arg: *const c_void) {
-        eprintln!(
-            "runtime__exceptionHandler called, don't know which circuit or how to parse the argument"
-        );
-    }
-
-    #[unsafe(no_mangle)]
-    #[allow(non_snake_case)]
-    extern "C" fn runtime__printErrorMessage(_arg: *const c_void) {
-        eprintln!(
-            "runtime__printErrorMessage called, don't know which circuit or how to parse the argument"
-        );
-    }
-
-    #[unsafe(no_mangle)]
-    #[allow(non_snake_case)]
-    extern "C" fn circuit_runtime__exceptionHandler(
-        circuit_name: *const c_char,
-        _arg: *const c_void,
-    ) {
-        // SAFETY: Our codegen always sets this to a const string.
-        let circuit_name = unsafe { CStr::from_ptr(circuit_name) };
-        let circuit_name = circuit_name.to_string_lossy();
-        eprintln!("[{circuit_name}] exceptionHandler called")
-    }
-
-    #[unsafe(no_mangle)]
-    #[allow(non_snake_case)]
-    extern "C" fn circuit_log_signal(
-        circuit_name: *const c_char,
-        _instance: *const c_void,
-        len: u32,
-        data: *const u32,
-    ) {
-        // SAFETY: Our codegen always sets this to a const string.
-        let circuit_name = unsafe { CStr::from_ptr(circuit_name) };
-        let circuit_name = circuit_name.to_string_lossy();
-        // SAFETY: Our codegen always sets this to a pointer to the stack (even if len==0)
-        let data = unsafe { std::slice::from_raw_parts(data, len as usize) };
-
-        let value = num_bigint::BigUint::from_slice(data);
-
-        eprintln!("[{circuit_name}] {value}");
-    }
-    // See https://github.com/iden3/snarkjs/blob/9a8f1c0083d18b9b5e18f526cfd729e7259423be/test/circuit2/circuit_js/witness_calculator.cjs#L44
-    #[unsafe(no_mangle)]
-    #[allow(non_snake_case)]
-    extern "C" fn circuit_log_message(
-        circuit_name: *const c_char,
-        _instance: *const c_void,
-        _typ: u32,
-        message: *const c_char,
-    ) {
-        // SAFETY: Our codegen always sets this to a const string.
-        let circuit_name = unsafe { CStr::from_ptr(circuit_name) };
-        let circuit_name = circuit_name.to_string_lossy();
-        // SAFETY: Our codegen always gives us a valid pointer to the stack, with 0 at the end.
-        let message = unsafe { CStr::from_ptr(message) };
-        let message = message.to_string_lossy();
-        let message = message.strip_suffix('\n').unwrap_or(&message);
-        if message.len() > 0 {
-            eprintln!("[{circuit_name}] {message}");
-        }
-    }
-}
 
 // Configuration of the circuit
 const MAX_PAYLOAD_BYTES: usize = 1024;
---
+++ src/witness/runtime.rs
@@ -0,0 +1,67 @@
+//! Functions rust_witness normally implements as a NOP.
+
+use std::ffi::{CStr, c_char, c_void};
+
+#[unsafe(no_mangle)]
+#[allow(non_snake_case)]
+extern "C" fn runtime__exceptionHandler(_arg: *const c_void) {
+    eprintln!(
+        "runtime__exceptionHandler called, don't know which circuit or how to parse the argument"
+    );
+}
+
+#[unsafe(no_mangle)]
+#[allow(non_snake_case)]
+extern "C" fn runtime__printErrorMessage(_arg: *const c_void) {
+    eprintln!(
+        "runtime__printErrorMessage called, don't know which circuit or how to parse the argument"
+    );
+}
+
+#[unsafe(no_mangle)]
+#[allow(non_snake_case)]
+extern "C" fn circuit_runtime__exceptionHandler(circuit_name: *const c_char, _arg: *const c_void) {
+    // SAFETY: Our codegen always sets this to a const string.
+    let circuit_name = unsafe { CStr::from_ptr(circuit_name) };
+    let circuit_name = circuit_name.to_string_lossy();
+    eprintln!("[{circuit_name}] exceptionHandler called")
+}
+
+#[unsafe(no_mangle)]
+#[allow(non_snake_case)]
+extern "C" fn circuit_log_signal(
+    circuit_name: *const c_char,
+    _instance: *const c_void,
+    len: u32,
+    data: *const u32,
+) {
+    // SAFETY: Our codegen always sets this to a const string.
+    let circuit_name = unsafe { CStr::from_ptr(circuit_name) };
+    let circuit_name = circuit_name.to_string_lossy();
+    // SAFETY: Our codegen always sets this to a pointer to the stack (even if len==0)
+    let data = unsafe { std::slice::from_raw_parts(data, len as usize) };
+
+    let value = num_bigint::BigUint::from_slice(data);
+
+    eprintln!("[{circuit_name}] {value}");
+}
+// See https://github.com/iden3/snarkjs/blob/9a8f1c0083d18b9b5e18f526cfd729e7259423be/test/circuit2/circuit_js/witness_calculator.cjs#L44
+#[unsafe(no_mangle)]
+#[allow(non_snake_case)]
+extern "C" fn circuit_log_message(
+    circuit_name: *const c_char,
+    _instance: *const c_void,
+    _typ: u32,
+    message: *const c_char,
+) {
+    // SAFETY: Our codegen always sets this to a const string.
+    let circuit_name = unsafe { CStr::from_ptr(circuit_name) };
+    let circuit_name = circuit_name.to_string_lossy();
+    // SAFETY: Our codegen always gives us a valid pointer to the stack, with 0 at the end.
+    let message = unsafe { CStr::from_ptr(message) };
+    let message = message.to_string_lossy();
+    let message = message.strip_suffix('\n').unwrap_or(&message);
+    if message.len() > 0 {
+        eprintln!("[{circuit_name}] {message}");
+    }
+}
