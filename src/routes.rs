38aaa946031d9e6ac33a69c0f1f14dded360b7a2
Jens <jens@perun.network>
2026-03-09 20:06:46 +0100

    Cleanup

--- src/routes.rs
+++ src/routes.rs
@@ -13,6 +13,8 @@ use sha2::Digest as _;
 
 use crate::sdjwt::{ISSUER_PRIVATE, ISSUER_PUBLIC};
 
+const DOMAIN: &str = "eudi2web3.erdstall.dev";
+
 pub fn build_router() -> Router {
     Router::new()
         .route("/request", post(request_proof))
@@ -25,68 +27,8 @@ struct RequestData {
     addr: String,
 }
 
-const DOMAIN: &str = "eudi2web3.erdstall.dev";
-
 /// Submit the address as the first step (will request a credential presentation)
 async fn request_proof(Json(data): Json<RequestData>) -> impl IntoResponse {
-    dbg!(data);
-
-    // Requirements: https://openid.net/specs/openid-4-verifiable-presentations-1_0.html#section-5.2-2.2
-    let nonce: String = rand::thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(20)
-        .map(char::from)
-        .collect();
-
-    //  credentials[].meta.vct_values contains a list of allowed credential types,
-    //  see https://openid.net/specs/openid-4-verifiable-presentations-1_0.html#appendix-B.3.5
-    let dcql = serde_json::to_string(&json!({
-        "credentials": [{
-            "id": "0",
-            "format": "dc+sd-jwt",
-            "meta": {
-                "vct_values": ["urn:eudi:pid:1"]
-            },
-            "claims": [
-                {"path": ["given_name"]}
-            ]
-
-        }]
-    }))
-    .unwrap();
-    let dcql = urlencoding::encode(&dcql);
-
-    let client_id = format!("https://{DOMAIN}/auth");
-    let client_id = urlencoding::encode(&client_id);
-
-    // Specification: https://openid.net/specs/openid-connect-core-1_0-31.html#SelfIssuedRequest
-    //  - REQUIRED scope: https://auth0.com/docs/get-started/apis/scopes/openid-connect-scopes#standard-claims
-    //  - REQUIRED response_type: vp_token tells the wallet we want a verifiable presentation
-    //  - REQUIRED client_id: Probably the unique identifier for the request, useful for the server
-    //
-    // Added/Changed by https://openid.net/specs/openid-4-verifiable-presentations-1_0.html#name-authorization-request
-    //  - REQUIRED dcql_query XOR scope
-    //  - REQURIED nonce
-    //  - REQUIRED client_id
-    //  - Somtimes required state
-    let url = format!(
-        // "openid-vp://?presentation_request_uri=https://127.0.0.1:8080/presentation_request/{}",
-        // binding_id
-        // "openid-vp://?presentation_request={request}",
-        // IMPORTANT: The x509_hash is not mine, it is random (for testing)
-        "\
-openid4vp://{DOMAIN}/auth?\
-client_id=redirect_uri%3Ahttps%3A%2F%2Fclient.example.org%2Fcb&\
-response_type=vp_token&\
-response_mode=direct_post&\
-nonce={nonce}&\
-dcql_query={dcql}"
-    );
-    // client_id=x509_hash:Uvo3HtuIxuhC92rShpgqcT3YXwrqRxWEviRiA0OZszk&\
-    // client_id=redirect_uri:https://example.com&\
-    //
-    // openid4vp://eudi2web3.erdstall.dev/auth?client_id=redirect_uri:https://example.com&response_type=vp_token&response_mode=direct_post&nonce=YYlLQppRsJvMzPH2UVR1&dcql_query=%7B%22credentials%22%3A%5B%7B%22id%22%3A%220%22%2C%22format%22%3A%22dc%2Bsd-jwt%22%2C%22meta%22%3A%7B%22vct_values%22%3A%5B%22urn%3Aeudi%3Apid%3A1%22%5D%7D%2C%22claims%22%3A%5B%7B%22path%22%3A%5B%22given_name%22%5D%7D%5D%7D%5D%7D
-
     let request_endpoint = format!("https://{DOMAIN}/api/request_jwt");
     let request_endpoint = urlencoding::encode(&request_endpoint);
 
