c98ea53dc8821e49ac71a7f5ed0e2f575d440650
Jens <jens@perun.network>
2026-04-08 16:03:20 +0200

    publish/cardano: Add tests

--- src/publish/cardano.rs
+++ src/publish/cardano.rs
@@ -1,13 +1,19 @@
 use circom_prover::prover::circom::{G1, G2};
 use hex::ToHex;
+use num_bigint::BigUint;
 use pallas_primitives::{Constr, Fragment, MaybeIndefArray, NetworkId, PlutusData};
-use pallas_txbuilder::{BuildConway, Input, Output, ScriptKind::PlutusV3, StagingTransaction};
+use pallas_txbuilder::{
+    BuildConway, ExUnits, Input, Output, ScriptKind::PlutusV3, StagingTransaction,
+};
 use pallas_wallet::PrivateKey;
 use serde::Deserialize;
 
 use crate::{MAX_VALUE_BYTES, prover::ProofWithPubInput};
 
 const BLOCKFROST_URL: &str = "https://cardano-preview.blockfrost.io/api/v0";
+
+// TODO: This file contains a bunch of methods using unwrap instead of having proper error
+// forwarding or retries.
 
 pub async fn deploy() {
     // Could be loaded once in the beginning and reused.
@@ -49,42 +55,19 @@ pub async fn deploy() {
     submit_tx(tx.tx_bytes.0).await;
 }
 
-// Returns the transaction hash
 pub async fn publish(proof: &ProofWithPubInput) -> [u8; 32] {
+    let redeemer = encode_redeemer(proof);
+    dbg!(hex::encode(&redeemer));
+    publish_inner(redeemer).await
+}
+
+// Returns the transaction hash
+async fn publish_inner(redeemer: Vec<u8>) -> [u8; 32] {
     // Could be loaded once in the beginning and reused.
     let addr = tokio::fs::read_to_string("me.addr").await.unwrap();
     let (script_bytes, script_hash) = script_bytecode().await;
 
-    // Prepare data
-    let claim_value = &proof.pub_input[1..1 + MAX_VALUE_BYTES];
-    let claim_value: Vec<u8> = claim_value
-        .iter()
-        .map(|x| {
-            let digits = x.to_u64_digits();
-            assert_eq!(digits.len(), 1);
-            assert!(digits[0] <= u8::MAX as u64);
-            x.to_u64_digits()[0] as u8
-        })
-        .collect();
-
     // Build inputs
-    let redeemer = PlutusData::Constr(Constr {
-        tag: 0,
-        any_constructor: None,
-        fields: MaybeIndefArray::Def(vec![
-            PlutusData::Constr(Constr {
-                tag: 0,
-                any_constructor: None,
-                fields: MaybeIndefArray::Def(vec![
-                    PlutusData::BoundedBytes(g1_to_bytes(&proof.proof.a).into()),
-                    PlutusData::BoundedBytes(g2_to_bytes(&proof.proof.b).into()),
-                    PlutusData::BoundedBytes(g1_to_bytes(&proof.proof.c).into()),
-                ]),
-            }),
-            PlutusData::BoundedBytes(claim_value.into()),
-        ]),
-    });
-    let redeemer = redeemer.encode_fragment().unwrap();
 
     let fee = 200_000;
     let min_balance = 4_000_000;
@@ -108,10 +91,15 @@ pub async fn publish(proof: &ProofWithPubInput) -> [u8; 32] {
                 .set_inline_script(PlutusV3, script_bytes)
                 .set_inline_datum(vec![0xd8, 0x79, 0x80]),
         )
-        .add_spend_redeemer(input_script.to_input(), redeemer, None)
-        .fee(fee)
+        .fee(fee);
+    let ex_units = eval_execution_units(&tx, input_script.to_input(), redeemer.clone()).await;
+    let tx = tx
+        .add_spend_redeemer(input_script.to_input(), redeemer, Some(ex_units))
         .build_conway_raw()
         .unwrap();
+
+    let tx_bytes = hex::encode(&tx.tx_bytes.0);
+    dbg!(tx_bytes);
 
     let sk = read_sk().await;
     // Sign changes tx.tx_bytes (adding the signature)
@@ -124,24 +112,77 @@ pub async fn publish(proof: &ProofWithPubInput) -> [u8; 32] {
     tx.tx_hash.0
 }
 
+fn encode_redeemer(proof: &ProofWithPubInput) -> Vec<u8> {
+    build_redeemer(proof).encode_fragment().unwrap()
+}
+
+fn build_redeemer(proof: &ProofWithPubInput) -> PlutusData {
+    let claim_value = &proof.pub_input[1..1 + MAX_VALUE_BYTES];
+    let claim_value: Vec<u8> = claim_value
+        .iter()
+        .map(|x| {
+            assert!(*x <= u8::MAX.into());
+            let digits = x.to_u64_digits();
+            let x = digits.first().copied().unwrap_or(0);
+            assert!(x <= u8::MAX as u64);
+            x as u8
+        })
+        .collect();
+
+    // See https://cardano-c.readthedocs.io/en/latest/api/plutus_data/constr_plutus_data.html
+    const TAG_CONSTR_0: u64 = 121;
+
+    PlutusData::Constr(Constr {
+        tag: TAG_CONSTR_0,
+        any_constructor: None,
+        fields: MaybeIndefArray::Def(vec![
+            PlutusData::Constr(Constr {
+                tag: TAG_CONSTR_0,
+                any_constructor: None,
+                fields: MaybeIndefArray::Def(vec![
+                    PlutusData::BoundedBytes(g1_to_bytes(&proof.proof.a).into()),
+                    PlutusData::BoundedBytes(g2_to_bytes(&proof.proof.b).into()),
+                    PlutusData::BoundedBytes(g1_to_bytes(&proof.proof.c).into()),
+                ]),
+            }),
+            PlutusData::BoundedBytes(claim_value.into()),
+        ]),
+    })
+}
+
 // TODO: I'm not sure if these are in the correct order. We will find out if the proof doesn't
 // verify.
 fn g1_to_bytes(a: &G1) -> Vec<u8> {
+    // to_bytes_be isn't guaranteed to return 32 bytes, so we need to pad accordingly.
     let mut bytes = Vec::with_capacity(3 * 32);
-    bytes.extend(a.x.to_bytes_be());
-    bytes.extend(a.y.to_bytes_be());
-    bytes.extend(a.z.to_bytes_be());
+    push_u256_be(&mut bytes, &a.x);
+    push_u256_be(&mut bytes, &a.y);
+    push_u256_be(&mut bytes, &a.z);
     bytes
 }
 fn g2_to_bytes(a: &G2) -> Vec<u8> {
     let mut bytes = Vec::with_capacity(6 * 32);
-    bytes.extend(a.x[0].to_bytes_be());
-    bytes.extend(a.x[1].to_bytes_be());
-    bytes.extend(a.y[0].to_bytes_be());
-    bytes.extend(a.y[1].to_bytes_be());
-    bytes.extend(a.z[0].to_bytes_be());
-    bytes.extend(a.z[1].to_bytes_be());
+    push_u256_be(&mut bytes, &a.x[0]);
+    push_u256_be(&mut bytes, &a.x[1]);
+    push_u256_be(&mut bytes, &a.y[0]);
+    push_u256_be(&mut bytes, &a.y[1]);
+    push_u256_be(&mut bytes, &a.z[0]);
+    push_u256_be(&mut bytes, &a.z[1]);
     bytes
+}
+
+/// Appends 32 bytes to `out` that contain the big-endian of num.
+///
+/// # Panicks
+/// Panicks if num does not fit into 32 bytes.
+fn push_u256_be(out: &mut Vec<u8>, num: &BigUint) {
+    // Use little endian, as big endian just calls that and reverses the Vec, too.
+    let num_bytes = num.to_bytes_be();
+    assert!(num_bytes.len() <= 32);
+    for _ in num_bytes.len()..32 {
+        out.push(0);
+    }
+    out.extend(num_bytes);
 }
 
 #[derive(Deserialize)]
@@ -202,7 +243,7 @@ impl Utxo {
     fn lovelace_balance(&self) -> u64 {
         let mut sum = 0;
         for e in &self.amount {
-            if e.unit == "locelace" {
+            if e.unit == "lovelace" {
                 sum += e.quantity.parse::<u64>().unwrap();
             }
         }
@@ -262,7 +303,7 @@ fn select_utxo_deployment<'a>(utxos: &'a [Utxo], script_hash: &str, min_balance:
         if u.reference_script_hash.is_some() {
             continue;
         }
-        if u.amount[0].quantity.parse::<u64>().unwrap() > min_balance {
+        if u.amount[0].quantity.parse::<u64>().unwrap() >= min_balance {
             return u;
         }
     }
@@ -274,36 +315,57 @@ fn select_utxo_publish<'a>(
     script_hash: &str,
     min_balance: u64,
 ) -> (&'a Utxo, &'a Utxo) {
-    let mut script = None;
-    let mut fee_payer;
+    let script = utxos
+        .iter()
+        .find(|u| u.reference_script_hash.as_deref() == Some(script_hash));
+    let fee_payer = utxos.iter().find(|u| {
+        u.reference_script_hash.is_none()
+            && u.amount[0].unit == "lovelace"
+            && u.amount[0].quantity.parse::<u64>().unwrap() >= min_balance
+    });
+    let Some(script) = script else {
+        panic!("Script not found");
+    };
+    let Some(fee_payer) = fee_payer else {
+        panic!("Insufficient funds");
+    };
+    (script, fee_payer)
+}
 
-    for u in utxos {
-        if u.reference_script_hash.as_deref() == Some(script_hash) {
-            script = Some(u);
-            continue;
-        }
-        if u.amount.is_empty() {
-            continue;
-        }
-        if u.amount[0].unit != "lovelace" {
-            continue;
-        }
-        if u.amount[0].quantity.parse::<u64>().unwrap() < min_balance {
-            continue;
-        }
-        fee_payer = Some(u);
-        let Some(script) = script else {
-            continue;
-        };
-        let Some(fee_payer) = fee_payer else {
-            continue;
-        };
-        return (script, fee_payer);
-    }
-    if script.is_none() {
-        panic!("Script not found")
-    }
-    panic!("Insufficient funds")
+/// I don't know how exactly this interacts with multiple redeemers or how the ex units would need
+/// to be set for that.
+async fn eval_execution_units(
+    tx: &StagingTransaction,
+    input: Input,
+    plutus_data: Vec<u8>,
+) -> ExUnits {
+    let blockfrost_key =
+        std::env::var("BLOCKFROST_KEY").expect("No BLOCKFROST_KEY environment variable");
+
+    dbg!(tx);
+    // I have not found a better way to do this, yet.
+    let tx = tx
+        .clone()
+        .add_spend_redeemer(input, plutus_data, Some(ExUnits { mem: 0, steps: 0 }))
+        .build_conway_raw()
+        .unwrap();
+
+    dbg!(hex::encode(&tx.tx_bytes));
+
+    let client = reqwest::Client::new();
+    let req = client
+        .post(format!("{BLOCKFROST_URL}/utils/txs/evaluate"))
+        .header("Content-Type", "application/cbor")
+        .header("project_id", blockfrost_key)
+        .body(tx.tx_bytes.0)
+        .build()
+        .unwrap();
+
+    dbg!(&req);
+    let res = client.execute(req).await.unwrap().text().await.unwrap();
+    eprintln!("{res}");
+
+    todo!()
 }
 
 async fn submit_tx(cbor: Vec<u8>) {
@@ -329,4 +391,98 @@ async fn submit_tx(cbor: Vec<u8>) {
     //     .unwrap();
 
     println!("{res}");
+}
+
+#[cfg(test)]
+mod test {
+    use circom_prover::prover::circom::{G1, G2, Proof};
+    use num_bigint::BigUint;
+    use pallas_primitives::{Fragment, PlutusData};
+
+    use super::*;
+    use crate::{MAX_VALUE_BYTES, prover::ProofWithPubInput};
+
+    #[test]
+    fn redeemer_encoding_roundtrip() {
+        let mut pub_input: Vec<BigUint> = Vec::with_capacity(1 + MAX_VALUE_BYTES);
+        pub_input.push(1u64.into());
+        for i in 0..MAX_VALUE_BYTES {
+            pub_input.push(i.into())
+        }
+
+        // Doesn't need to be an acruate/working proof, we are only checking encoding.
+        let proof = ProofWithPubInput {
+            proof: Proof {
+                a: G1 {
+                    x: 10u64.into(),
+                    y: 11u64.into(),
+                    z: 12u64.into(),
+                },
+                b: G2 {
+                    x: [20u64.into(), 30u64.into()],
+                    y: [21u64.into(), 31u64.into()],
+                    z: [22u64.into(), 32u64.into()],
+                },
+                c: G1 {
+                    x: 40u64.into(),
+                    y: 41u64.into(),
+                    z: 42u64.into(),
+                },
+                protocol: "groth16".to_owned(),
+                curve: "bn128".to_owned(),
+            },
+            pub_input,
+        };
+
+        let bytes = super::encode_redeemer(&proof);
+        dbg!(hex::encode(&bytes));
+
+        // Snippet from pallas-txbuilder `build_conway_raw`
+        let data = PlutusData::decode_fragment(bytes.as_ref())
+            .expect("Encoding roundtrip should not fail");
+        dbg!(data);
+    }
+
+    #[tokio::test]
+    async fn eval_execution() {
+        // Generated by running the project with `dbg!(hex::encode(&redeemer));` in publish.
+        let redeemer = hex::decode("d87982d879835f584008fffc3c784baebc90dc8b53c54c5df4cd22809da26b210f1e7473abb857cc252ccf30baf5a778305fcab756ac85e0eaf82c146534ae02e85d08933a64e3a76958200000000000000000000000000000000000000000000000000000000000000001ff5f584002bc44150398c624c333abf8401661159cf7a6843468cd95439c0411019fae1a01891603c5e26e592adfc4c9dddb3b0e6aaa78ce5a8f51663a1a2e671af00552584028df7cea046fcd5589a44e867672c0484595c394f159a6205f91da576f533fab08ac5ff0ed8a8fc4beb2c5aff94311a7d0a08e65a72cbd598465f6caf23fd93d584000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000ff5f5840273c104cf16d20ebc0fa910e925592fb5731bacf1caaf6a036b51da2d64e9c811d6b8fdb66d2dd4839713c337831d6f9ed3838efdb9e96ff30fb9585fa803e3658200000000000000000000000000000000000000000000000000000000000000001ff58402022666f6f6261723822000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000").unwrap();
+
+        // Sanity check
+        let data = PlutusData::decode_fragment(&redeemer).expect("Decoding should not fail");
+        dbg!(data);
+
+        let tx_hash = publish_inner(redeemer).await;
+        dbg!(hex::encode(tx_hash));
+
+        // let addr = tokio::fs::read_to_string("me.addr").await.unwrap();
+        // let (script_bytes, script_hash) = script_bytecode().await;
+        //
+        // let fee = 200_000;
+        // let min_balance = 4_000_000;
+        //
+        // // Use the first UTXO we have as input
+        // let utxos = get_utxos(&addr).await;
+        // let (input_script, input_fees) =
+        //     select_utxo_publish(&utxos, &script_hash, fee + min_balance);
+        // let addr = pallas_addresses::Address::from_bech32(&addr).unwrap();
+        //
+        // let tx = StagingTransaction::new()
+        //     .network_id(NetworkId::Testnet.into())
+        //     .input(input_script.to_input())
+        //     .input(input_fees.to_input())
+        //     .output(Output::new(
+        //         addr.clone(),
+        //         input_fees.lovelace_balance() - fee,
+        //     ))
+        //     .output(
+        //         Output::new(addr, input_script.lovelace_balance())
+        //             .set_inline_script(PlutusV3, script_bytes)
+        //             .set_inline_datum(vec![0xd8, 0x79, 0x80]),
+        //     )
+        //     .fee(fee);
+        //
+        // let ex_units = eval_execution_units(&tx, input_script.to_input(), redeemer).await;
+        // dbg!(&ex_units);
+    }
 }
